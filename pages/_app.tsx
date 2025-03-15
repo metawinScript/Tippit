0145aa3880c4eb2d2ec9bc9a7c603dc5876996ad
metawinScript <ml364674life@gmail.com>
2025-03-15 17:13:22 +0100

    configured wagmi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,57 +5,50 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import type { AppProps } from "next/app";
 
-import { connectorsForWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-//
-import { Chain, getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { connectorsForWallets, getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import type { Chain } from "viem";
+import { WagmiProvider } from "wagmi";
 
 import Layout from "../components/Layout";
-
-// Import the ToastContainer component from react-toastify to display notifications.
 import { ToastContainer } from "react-toastify";
 
-import { publicProvider } from "wagmi/providers/public";
-const okpoko: Chain = {
-    id: 0x500fad,
-    name: "Okpoko",
-    network: "okpoko",
-    iconUrl: "https://example.com/icon.svg",
-    iconBackground: "#fff",
+const teaAssam = {
+    id: 93384,
+    name: 'tea-assam',
     nativeCurrency: {
         decimals: 18,
-        name: "OKPOKO",
-        symbol: "OKP",
+        name: 'Tea',
+        symbol: 'TEA',
     },
     rpcUrls: {
-        public: { http: ["https://froopyland.dymension.xyz/25/okpoko_5246893-1/evmrpc"] },
-        default: { http: ["https://froopyland.dymension.xyz/25/okpoko_5246893-1/evmrpc"] },
+        public: {
+            http: ['https://assam-rpc.tea.xyz'],
+        },
+        default: {
+            http: ['https://assam-rpc.tea.xyz'],
+        },
     },
-  
+    blockExplorers: {
+        default: {
+            name: 'tea-assam',
+            url: 'https://assam.tea.xyz',
+        },
+    },
+
     testnet: false,
-};
-const { provider, chains } = configureChains([okpoko], [publicProvider()]);
+} as const satisfies Chain;
 
-
-const { connectors } = getDefaultWallets({
-    appName: "Tippit",
-    projectId: "f2b6bcbe27485bc2aa6c3c619c9e7165",
-    chains,
-});
-
-// Create the Wagmi client.
-const wagmiClient = createClient({
-    autoConnect: true,
-    connectors,
-    provider,
+const wagmiConfig = getDefaultConfig({
+    appName: "tippit",
+    projectId: "",
+    chains: [teaAssam]
 });
 
 // Create and export the App component wrapped with the RainbowKitProvider and WagmiConfig.
 function App({ Component, pageProps }: AppProps) {
     return (
-        <WagmiConfig client={wagmiClient}>
+        <WagmiProvider client={wagmiConfig}>
             <RainbowKitProvider chains={chains} coolMode={true}>
                 <ToastContainer position={"bottom-center"} />
                 <Layout>
