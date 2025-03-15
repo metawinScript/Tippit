bf15b3f5aa404dd077de540460cef41fe48342b9
metawinScript <ml364674life@gmail.com>
2025-03-15 17:03:08 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,15 @@
-// This file is the entry point for the app. It is used to wrap the app with the RainbowKitProvider and WagmiConfig components.
 
-// Import the global style sheet as well as the RainbowKit and react-toastify stylesheets.
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 
 import type { AppProps } from "next/app";
 
-// Import the connectorsForWallets function to create a list of wallets to connect to.
-// Import the RainbowKitProvider component to wrap the app with.
 import { connectorsForWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-
-// Import three different wallets connectors from the RainbowKit package.
-import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
 //
 import { Chain, getDefaultWallets } from "@rainbow-me/rainbowkit";
-// Import configureChains, createClient, and WagmiConfig from the Wagmi package to configure the Wagmi client.
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 
-// Import the jsonRpcProvider from the Wagmi package to specify the RPC URLs of the chains we want to connect to.
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
 import Layout from "../components/Layout";
