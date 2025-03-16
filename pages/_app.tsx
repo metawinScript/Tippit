06f4e1878963325aa1914f3d9ecf8961156c753d
metawinScript <ml364674life@gmail.com>
2025-03-16 16:41:27 +0100

    installed query client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,14 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import type { AppProps } from "next/app";
 
-import { connectorsForWallets, getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 import type { Chain } from "viem";
 import { WagmiProvider } from "wagmi";
 
 import Layout from "../components/Layout";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const teaAssam = {
     id: 93384,
@@ -41,20 +42,24 @@ const teaAssam = {
 
 const wagmiConfig = getDefaultConfig({
     appName: "tippit",
-    projectId: "",
+    projectId: "jkionbv",
     chains: [teaAssam]
 });
+
+const qClient = new QueryClient();
 
 // Create and export the App component wrapped with the RainbowKitProvider and WagmiConfig.
 function App({ Component, pageProps }: AppProps) {
     return (
         <WagmiProvider config={wagmiConfig}>
+            <QueryClientProvider client={qClient}>
             <RainbowKitProvider>
                 <ToastContainer position={"bottom-center"} />
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
             </RainbowKitProvider>
+            </QueryClientProvider>
         </WagmiProvider>
     );
 }
